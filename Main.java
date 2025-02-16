c684b7fb4150b55a47dd3409abec842ec34cdf73
EllisFitzUSM <ellis.fitzgerald@maine.edu>
2025-02-15 23:16:49 -0500

    Add Gitignore and Main

---
+++ Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Ellis Fitzgerald
+ * @version February 15, 2025,
+ * Lab 3 Program File
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
